50f83c1f502256934958099f5431177b6ea5ff96
David Moodie <davidmoodie12@gmail.com>
2016-10-16 09:36:19 +0100

    Remove code

--- allergenSorter.js
+++
@@ -1,9 +0,0 @@
-function sortAllergens(json) {
-	var red = [];
-	var yellow = [];
-	json.allergens.forEach(function(al){
-		if (al.allergen_red_ingredients != "") { red.push(al.allergen_name); }
-		if (al.allergen_yellow_ingredients != "") { yellow.push(al.allergen_name); }
-	});
-	return { 'red':red, 'yellow':yellow };
-}
--- publish.sh
+++
@@ -1,5 +0,0 @@
-rm index.zip
-cd src
-zip -X -r ../index.zip *
-cd ..
-aws lambda update-function-code --function-name myAllergySkill --zip-file fileb://index.zip
